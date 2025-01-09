0cfdee13790ebe0112d69619e4d90b7894b5c797
idoschw3 <ido.schwartz.il@gmail.com>
2025-01-09 20:05:40 +0200

    Update pandas_utils.py
    update output directory path

--- large_scale_data/pandas_utils.py
+++ large_scale_data/pandas_utils.py
@@ -11,12 +11,12 @@ def convert_excel_to_csv(uploaded_file, sheet_name = None, output_dir='./output/
         if isinstance(excel_data, dict):
             csv_files = []
             for name, df in excel_data.items():
-                csv_file = f"{name}_output.csv"
+                csv_file = os.path.join(output_dir, f"{name}_output.csv")
                 df.to_csv(csv_file, index=False)
                 csv_files.append(csv_file)
             return csv_files
         else:
-            csv_file = "output.csv"
+            csv_file = os.path.join(output_dir, "output.csv")
             excel_data.to_csv(csv_file, index=False)
             return csv_file
 
