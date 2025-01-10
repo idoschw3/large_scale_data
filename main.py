1b757b9fa4b1feaa6b717157347e283e22ec6c75
idoschw3 <ido.schwartz.il@gmail.com>
2025-01-10 11:00:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pandas as pd
 import streamlit as st
-from large_scale_data.pandas_utils import convert_excel_to_csv
-
+from large_scale_data.pandas_utils import convert_excel_to_csv, plot_data
 
 st.title("Large Scale Data Analyzer")
 
-uploaded_file = st.file_uploader("Upload your data (excel files only, .xlsx or .xls)", type=["xlsx", "xls"])
-st.write("Make sure your data is well structured, with titles for each column.")
-
 sheet_name = st.text_input("If you wish to work only with a specific sheet in your data specify it here (case sensitive)")
+
+uploaded_file = st.file_uploader("Upload your data (excel files only, .xlsx or .xls)", type=["xlsx", "xls"])
+st.markdown("""
+Make sure your data is well structured, with titles for each column.
+  
+Make sure you understand your data. Your graphs will not make sense if you choose the wrong plot type.  
+For example - in the diamond data set, plotting price against carat using a line plot won't yield a sensible graph, you will need to use a different type of graph.
+""")
 
 data = None
 df = None
+x_axis = None
+y_axis = None
 
 if uploaded_file:
     try:
@@ -45,8 +51,21 @@ with st.container():
             help="column"
         )
         if x_axis:
-            y_axis = st.multiselect(
+            y_axes = st.multiselect(
             "What column are your y-axes?",
             df.columns,
             help="column"
         )
+    if x_axis and y_axes:
+        columns_per_row = 2
+
+        for i, y_axis in enumerate(y_axes):
+            if i % columns_per_row == 0:
+                cols = st.columns(columns_per_row)
+
+            fig = plot_data(df, x_axis, y_axis)
+
+            col_index = i % columns_per_row
+            with cols[col_index]:
+                st.pyplot(fig)
+
