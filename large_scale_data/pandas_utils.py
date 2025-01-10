7bc57e9c4e563633d3178389e7ec2c16077b8e5e
idoschw3 <ido.schwartz.il@gmail.com>
2025-01-10 10:59:54 +0200

    Update pandas_utils.py
    adding function to plot basic graphs

--- large_scale_data/pandas_utils.py
+++ large_scale_data/pandas_utils.py
@@ -2,6 +2,9 @@ import pandas as pd
 import streamlit as st
 import openpyxl
 import os
+import matplotlib.pyplot as plt
+from matplotlib.lines import lineStyles
+
 
 @st.cache_data
 def convert_excel_to_csv(uploaded_file, sheet_name = None, output_dir='./output/'):
@@ -24,3 +27,20 @@ def convert_excel_to_csv(uploaded_file, sheet_name = None, output_dir='./output/
         print(f"Error converting Excel to CSV: {e}")
         return None
 
+def plot_data(df, x_axis, y_axis):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    if pd.api.types.is_numeric_dtype(df[y_axis]):
+        ax.plot(df[x_axis], df[y_axis], marker = 'o', linestyle = '-')
+        ax.set_title(f"line plot for {y_axis}")
+        ax.set_xlabel(x_axis)
+        ax.set_ylabel(y_axis)
+    else:
+        categories = df[y_axis].value_counts()
+        ax.bar(categories.index, categories.values, label=y_axis)
+        ax.set_title(f"Bar Chart for {y_axis}")
+        ax.set_xlabel(y_axis)
+        ax.set_ylabel("Count")
+
+    ax.legend()
+    plt.tight_layout()
+    return fig
